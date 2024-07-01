bdee38e49145e1b83d2c7fa8af925d971c365aeb
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-01 10:11:38 -0400

    Update chapter_six.py

--- chapter_six.py
+++ chapter_six.py
@@ -1,8 +1,10 @@
 alien_o = {'color':'green','points':5}
 print(alien_o['color'])
 print(alien_o['points'])
+#Running code to get information from list
 new_points = alien_o['points']
 print("You just eraned " + str(new_points)+" points!")
+#Adding new key to list
 alien_o['x_position']=0
 alien_o['y_position']=25
 print(alien_o)
