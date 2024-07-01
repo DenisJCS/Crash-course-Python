0067627e7257bef9b28cd7ddd2cd630ef3cd7d44
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-01 10:50:54 -0400

    Add files via upload

---
+++ favorite_language.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',
+    'alex':'js',
+ }
+print("Sarah's favorite language is "+
+      favorite_languages['sarah'].title()+".")
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is " +str(language)+".")
