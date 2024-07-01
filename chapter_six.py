cd45f1b84d1f7da3c0caf69e6b9dbf7a73b2577d
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-01 10:10:31 -0400

    Alien

---
+++ chapter_six.py
@@ -0,0 +1,8 @@
+alien_o = {'color':'green','points':5}
+print(alien_o['color'])
+print(alien_o['points'])
+new_points = alien_o['points']
+print("You just eraned " + str(new_points)+" points!")
+alien_o['x_position']=0
+alien_o['y_position']=25
+print(alien_o)
