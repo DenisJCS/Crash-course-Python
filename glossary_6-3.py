597fb0b175896ad2120085bf4b694cbd5b7c28e0
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-03 09:50:48 -0400

    Add files via upload

---
+++ glossary_6-3.py
@@ -0,0 +1,19 @@
+#Glossary 
+glossary = {
+    'if':'condition',
+    'in':'present',
+    'num':'amount',
+    'del':'delete',
+    'sort':'from a to z',
+    'sort revers':'backword',
+    'upper':'high register',
+    'lower':'lower register',
+    'for':'loop',
+    'elif':"second if",
+    'list':'create a list'
+}
+print(glossary)
+for term,definition in glossary.items():
+    print(f"{term}:\n{definition}\n")
+
+
