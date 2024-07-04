6d76ff198c5b6b38d61b139244da86664c8a2382
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-04 09:50:12 -0400

    Update favorite_language.py

--- favorite_language.py
+++ favorite_language.py
@@ -13,3 +13,20 @@ print(f"Sarah's favorite language is " +str(language)+".")
 for key,value in favorite_languages.items():
     print("\nKey:"+key)
     print("Value:"+value)
+
+for name,language in favorite_languages.items():
+    print(name.title()+" s favorite language is "+language.title())
+
+for name in favorite_languages.keys():
+    print(name.title())
+#Take all names from the dictionary
+#and when right name appears will show the message
+friends = ['phil','sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        print("Hi " + name.title()+ 
+              ", I see your favorite language is "+
+              favorite_languages[name].title()+"!")
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
