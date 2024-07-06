f9af81b89f7e08c6b4caa9381165ebc6467bf2f4
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-05 20:56:12 -0400

    Update neseting.py

--- neseting.py
+++ neseting.py
@@ -20,3 +20,18 @@ for alien in aliens[:5]:
 #Show how many aliens have been created.
 print(f"Total number of aliens: {len(aliens)}")
 
+#Change some aliens in the list
+#Make an empty list for storing aliens
+for alien_number in range(30):
+    new_alien = {'color':'green','points':5, 'speed':'slow'}
+    aliens.append(new_alien)
+for alien in aliens[:3]:
+    if alien ['color'] == 'green':
+       alien ['color'] = 'yellow'
+       alien ['speed'] = 'medium'
+       alien ['points'] = 10
+
+#Show the first five aliens    
+for alien in aliens[:5]:
+    print(alien)
+print(f"Total number of aliens: {len(aliens)}")
