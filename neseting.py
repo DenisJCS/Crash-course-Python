c79d42c15fce20f734d89deca17f6ab12af20e9d
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-05 21:02:51 -0400

    Update neseting.py

--- neseting.py
+++ neseting.py
@@ -30,6 +30,10 @@ for alien in aliens[:3]:
        alien ['color'] = 'yellow'
        alien ['speed'] = 'medium'
        alien ['points'] = 10
+    elif alien ['color'] == 'yellow':
+         alien ['color'] = 'red'
+         alien ['speed'] = 'fast'
+         alien ['points'] = 15
 
 #Show the first five aliens    
 for alien in aliens[:5]:
