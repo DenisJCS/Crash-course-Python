76d77ac0a0d48a09a094eaf1b52d7ff498a4efcf
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-05 20:46:40 -0400

    Add files via upload

---
+++ neseting.py
@@ -0,0 +1,22 @@
+#Dictionary is below
+alien_0 = {'color':"green",'points':5}
+alien_1 = {'color':'yellow','points':10}
+alien_2 = {'color':'red','points':15}
+#Now will nest it in list
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
+aliens = []
+#Make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color':'green','points':5, 'speed':'slow'}
+    aliens.append(new_alien)
+
+#Show first five aliens    
+for alien in aliens[:5]:
+    print(alien)
+
+#Show how many aliens have been created.
+print(f"Total number of aliens: {len(aliens)}")
+
