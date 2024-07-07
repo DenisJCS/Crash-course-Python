b4d40bea03ab2baa5ce103c07d7914f6e760ed70
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-06 21:41:12 -0400

    Add files via upload

---
+++ many_users.py
@@ -0,0 +1,19 @@
+users = {
+    'aeinstiain':{
+        'first':'albert',
+        'last':'einstein',
+        'location':'princeton',
+    },
+    'mcurie':{
+        'first':'marie',
+        'last':'curie',
+        'location':'paris',
+    },
+}
+for username,user_info in users.items():
+    print(f"\nusername : {username}")
+    full_name = f"{user_info['first']}{user_info['last']}"
+    location = user_info['location']
+    print(f"\tFull name:{full_name.title()}")
+    print(f"\tlocation:{location.title()}")
+    
