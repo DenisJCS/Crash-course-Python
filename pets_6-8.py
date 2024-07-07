bd4da2f94c1f966f6bf1bf86a856b4db42de2107
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-06 22:29:45 -0400

    Add files via upload

---
+++ pets_6-8.py
@@ -0,0 +1,30 @@
+pets = {
+    'dog':{
+        'animal':'dog',
+        'kind':'ovcharka',
+        'name':'jairan',
+        'age':4,
+        'owner':'alex',
+        'location':'vilige',
+    },
+    'horse':{
+        'animal':'horse',
+        'kind':'mountain',
+        'name':'maximus',
+        'age':5,
+        'owner':'luke',
+        'location':'farm',
+    },
+    
+}
+for pet,pets_info in pets.items():
+    print(f"\nPet : {pet}")
+    animal_info = f"{pets_info['animal']} {pets_info['kind']}"
+    name_age = f"{pets_info['name']+":"} {pets_info['age']}"
+    owner = f"{pets_info['owner']}"
+    location = f"{pets_info['location']}"
+    print(f"\tAnimal info : {animal_info.title()}")
+    print(f"\tName and age of animal :{name_age.title()}")
+    print(f"\tOwner is : {owner.title()}")
+    print(f"\tLocation is :{location.title()}")
+
