429d656e116e1d4e8294f89fe58a0201076501fe
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-09 21:56:11 -0400

    Update chapter7.py

--- chapter7.py
+++ chapter7.py
@@ -1,2 +1,9 @@
 name = input("Please enter your name: ")
 print(f"\nHello,{name}!")
+
+#greeting
+prompt = ("If you share your name, we can personalize this message you see.")
+prompt += ("\nWhat is your first name?")
+
+name = input(prompt)
+print(f"\nHello, {name}!")
