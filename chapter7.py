127ec7b6f01b5a83ca834e4bf008a4caef0d681c
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-09 22:14:29 -0400

    Update chapter7.py

--- chapter7.py
+++ chapter7.py
@@ -29,3 +29,14 @@ How old are you? 21
 >>> age >= 18
 True
 >>> 
+
+
+rollecoaster.py
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >=48:
+    print("\nYou are tall enough to ride.")
+else:
+    print("\nYou'll be able to ride, when you're a little older.")
+        
