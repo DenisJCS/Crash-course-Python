5e11597166b97045c96d88114c29596aa83832d7
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-11 09:36:45 -0400

    Update chapter7.py

--- chapter7.py
+++ chapter7.py
@@ -126,4 +126,15 @@ while active:
     else:
         print(message)
             
-
+Using Continue in a Loop
+current_number = 0
+while current_number <10 :
+    current_number +=1
+    if current_number %2 ==0:
+        continue
+    print(current_number)
+  1
+  3
+  5
+  7
+  9
