805e9869fb2b370902e70d8e2ac394cf20cac99a
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-13 10:13:42 -0400

    Add files via upload

---
+++ blablabla.py
@@ -0,0 +1,10 @@
+#Has conditions to run code propery
+current_number = 1
+while current_number <=5:
+    print(current_number)
+    current_number += 1
+#Infinite loop never ends
+current_number = 1
+while current_number <=5:
+    print(current_number)
+    
