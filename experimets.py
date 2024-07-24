00b1f41190aa8b2d57d783f61d6a7c213622070a
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-23 22:23:00 -0400

    Update experimets.py

--- experimets.py
+++ experimets.py
@@ -1,1 +1,10 @@
 #Code below
+help("string")
+
+def printing_model_design(unprinted_models,completed_models):
+    """Print models moving each item to completed_models"""
+    while unprinted_models:
+        current_desing = unprinted_models.pop()
+        print(f"Printing {current_desing}")
+        completed_models.append(current_desing)
+        
